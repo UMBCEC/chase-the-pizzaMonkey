a3d068ad7b0a70d951eabcbf395fd16a5c6e5263
UMBCEC <83308443+UMBCEC@users.noreply.github.com>
2021-04-29 10:32:40 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    pizza.setPosition(randint(0, 160), randint(0, 120))
+    info.startCountdown(10)
+})
+let pizza: Sprite = null
+scene.setBackgroundColor(13)
+pizza = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+let mySprite = sprites.create(img`
+    . . . . . f f f f f . . . . . . 
+    . . . . f e e e e e f . . . . . 
+    . . . f d d d d d e e f . . . . 
+    . . f f f d d f f d e f f . . . 
+    . c d d e e d d d d e d d f . . 
+    . c c d d d d c d d e d f f f . 
+    . c d c c c c d d d e d f b d f 
+    . . c d d d d d d e e f f d d f 
+    . . . c d d d d e e f f e f f f 
+    . . . . f f f e e f e e e f . . 
+    . . . . f e e e e e e e f f f . 
+    . . . f e e e e e e f f f e f . 
+    . . f f e e e e f f f f f e f . 
+    . f b d f e e f b b f f f e f . 
+    . f d d f e e f d d b f f f f . 
+    . f f f f f f f f f f f f f . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
